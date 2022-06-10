5da6efeb155f7057da165f0b4d4989818992b2cf
aidanabekova <bekovaaidana081003@gmail.com>
2022-06-10 21:20:29 +0600

    first commit

---
+++ lesson1/app.js
@@ -0,0 +1,55 @@
+const a = "abc"; //string
+
+let c = 32;  //number
+
+const bool = true;
+const bool2 = false; //boolean
+
+// console.log(typeof c)
+// console.log(typeof a)
+const obj = {  //object
+    key: "value",
+    username: "Bekova Aidana",
+    data: {
+        first_name: "Lake",
+        last_name: "Rus"
+    }
+}
+// console.log(obj.username)
+// console.log(obj.data.last_name)
+
+// const arr = ["DADA", 234, true, false]
+// console.log(arr[2], arr[0])
+// console.log(arr)
+// console.log(typeof arr)
+
+// const name = prompt("What is your name?")
+// console.log("Hello" + name)
+// const name = alert("нельзя!")
+// console.log(name)
+// const first_number = prompt('First number?')
+// const last_number = prompt('Last number?')
+// if(first_number < last_number){
+//     console.log(first_number + "<" + last_number)
+// } else if(first_number > last_number){
+//     console.log(first_number + ">" + last_number)
+// }else {
+//     console.log(first_number + "=" + last_number)
+// }
+
+// const age = prompt('How old are you?')
+//
+// if(age < 18){
+//     console.log('вам нельзя!')
+// } else if (age > 18){
+//     console.log('вам можно!')
+// } else {
+//     console.log('спорно')
+// }
+
+const arr2 = [[[[[[[[[[[[[[[5]]]]]]]]]]]]]]]
+console.log(arr2[0][0][0][0][0][0][0][0][0][0][0][0][0][0][0])
+
+
+
+
---
+++ lesson2/lesson2.js
@@ -0,0 +1,126 @@
+// const array = [
+//     {
+//         username: 'Jack',
+//         full_name: 'Lack Koin'
+//     },
+//     {
+//         username: 'Jack',
+//         full_name: 'Lack Koin'
+//     },
+//     {
+//         username: 'Jack',
+//         full_name: 'Lack Koin'
+//     },
+//     {
+//         username: 'Jack',
+//         full_name: 'Lack Koin'
+//     },
+//     {
+//         username: 'Jack',
+//         full_name: 'Lack Koin'
+//     },
+//     {
+//         username: 'Jack',
+//         full_name: 'Lack Koin'
+//     },
+//     {
+//         username: 'Jack',
+//         full_name: 'Lack Koin'
+//     }
+//
+// ]
+// инкрементация
+// let i = 0; //i++
+// i = i + 1
+//
+// for(let i = 0; i < array.length; i++){
+//     console.log(array[i])
+// }
+// for(let i = 0; i < array.length; i++){
+//     if(i === 2){                                   //это универсальный for, чаще всего используется
+//         break;
+//     } else {
+//         console.log(array[i])
+//     }
+// }
+//
+// for(let user of array){
+//     console.log(user)
+// }
+//
+// const obj = {
+//     key1: 'b1',
+//     key2: 'b2',
+//     key3: 'b3',
+//     key4: 'b4',
+//     key5: 'b5',
+//     key6: 'b6',
+// }
+// for(let key in obj){
+//     console.log(obj[key])
+// }
+// let i = 0
+// while(i < 5){
+//     console.log('Hello world')
+//     i++
+// }
+
+// const users = [
+//     {
+//         username: 'Jack',
+//         salary: 500
+//     },
+//     {
+//         username: 'John',
+//         salary: 5000
+//     },
+//     {
+//         username: 'Aibek',
+//         salary: 10000
+//     }
+// ]
+
+
+// const filteredUsers = users.filter(user => user.salary > 500)
+// console.log(filteredUsers)
+// const filteredUsers = []
+
+// for(let user of users){
+//     if(user.salary > 500){
+//         filteredUsers.push()
+//     }
+// }
+// console.log(filteredUsers)
+
+// const destructUsers = users.map(user => ({
+//     name: user.username,
+//     salary: user.salary
+// }))
+// console.log(destructUsers)
+
+// const robot = [
+//     {
+//         name: 'CCC',
+//         value: 350
+//     },
+//     {
+//         name: 'DDD',
+//         value: 5000
+//     },
+//     {
+//         name: 'HHH',
+//         value: 10000
+//     },
+//     {
+//         name: 'KKKK',
+//         value: 9000
+//     },
+//     {
+//         username: 'BBB',
+//         value: 100000
+//     }
+// ]
+// const filterRobot = robot.filter(robt => robt.value > 9000)
+// console.log(filterRobot)
+//
+//
